53d02a40ac957549c7a7f8476ea7a713acf62db6
sumitprasadsaha-cyber <sumitprasadsaha@gmail.com>
2026-07-15 04:29:42 +0000

    Add Android build workflow

---
+++ android/app/src/main/java/com/sumitprasadsaha/tuitionmanager/MainActivity.java
@@ -0,0 +1,5 @@
+package com.sumitprasadsaha.tuitionmanager;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.sumitprasadsaha.tuitionmanager',
+  appName: 'Tuition Manager',
+  webDir: 'dist'
+};
+
+export default config;
